ec6bdff73d7c88d66f30dbac3f86a948530b1260
parwezalam9582 <parwezalam9582@gmail.com>
2024-08-28 22:28:35 +0530

    Trigger

--- 2Sum.py
+++ 2Sum.py
@@ -12,9 +12,10 @@ def predict():
         Number1 = int(data.get('Number1', 0))
         Number2 = int(data.get('Number2', 0))
         Number3 = int(data.get('Number3', 0))
+        Number4 = int(data.get('Number4', 0))
         
         # Calculate the sum of the two numbers
-        answer = Number1 + Number2 +Number3
+        answer = Number1 + Number2 +Number3 +Number4
         
         # Return the result as a JSON response
         return jsonify({'answer': answer})
