8d8fb64aaec695833be02fdf1156f03e2a257c52
parwezalam9582 <parwezalam9582@gmail.com>
2024-08-28 18:24:52 +0530

    Your commit message here

--- 2Sum.py
+++ 2Sum.py
@@ -11,9 +11,10 @@ def predict():
         # Extract and convert the numbers from the received JSON data
         Number1 = int(data.get('Number1', 0))
         Number2 = int(data.get('Number2', 0))
+        Number3 = int(data.get('Number3', 0))
         
         # Calculate the sum of the two numbers
-        answer = Number1 + Number2
+        answer = Number1 + Number2 +Number3
         
         # Return the result as a JSON response
         return jsonify({'answer': answer})
